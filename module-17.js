1f460fd4ef4f9b6c820e52a4a40f7063dc073f45
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-08-09 21:10:06 +0600

    smallest number added

--- module-17.js
+++ module-17.js
@@ -45,21 +45,46 @@ You are given three numbers 13, 79, and 45. Write a program that will print the
 largest number using if-else.
 */
 
-var num1 = 13;
-var num2 = 79;
-var num3 = 45;
+// var num1 = 13;
+// var num2 = 79;
+// var num3 = 45;
 
-if (num1 > num2) {
-    if (num1 > num3) {
-        console.log(num1);
+// if (num1 > num2) {
+//     if (num1 > num3) {
+//         console.log(num1);
+//     }
+//     else {
+//         console.log(num3)
+//     }
+// }
+// else if (num2 > num3) {
+//     console.log(num2)
+// }
+// else {
+//     console.log(num3)
+// }
+
+
+
+
+
+// find the smallest number
+
+var number1 = 7;
+var number2 = 13;
+var number3 = 3;
+
+if (number1 < number2) {
+    if (number1 < number3) {
+        console.log('smallest number',number1)
     }
     else {
-        console.log(num3)
+        console.log(number3)
     }
 }
-else if (num2 > num3) {
-    console.log(num2)
+else if (number2 < number3) {
+    console.log(number2);
 }
 else {
-    console.log(num3)
+    console.log(number3)
 }
